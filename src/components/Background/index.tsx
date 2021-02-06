fd41e268cda6b9e5f3a60db7ee5705b768f032ad
NoronhaGCP <inc.691@gmail.com>
2021-02-06 13:21:33 -0300

    feat: background

---
+++ src/components/Background/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container, TopSide } from './styles';
+
+const Background: React.FC = () => {
+  return (
+    <Container>
+        <TopSide></TopSide>
+    </Container>
+  );
+}
+
+export default Background;
---
+++ src/components/Background/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
+  z-index: -50;
+`;
+
+export const TopSide = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 50%;
+  background: #f1f5fe;
+  border-bottom-left-radius: 200px;
+  z-index: -50;
+`
