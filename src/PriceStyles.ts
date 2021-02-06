ede59c6409d35a9d53eca83ae2d71a80a7418536
NoronhaGCP <inc.691@gmail.com>
2021-02-06 18:58:16 -0300

    feat: show prices

---
+++ src/PriceStyles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Price = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 60px 0;
+    color: #8d909f;
+    font-weight: 600;
+`
+
+export const Content = styled.span`
+    color: #22304d;
+    font-size: 40px;
+    font-weight: bold;
+    margin-right: 12px;
+`
